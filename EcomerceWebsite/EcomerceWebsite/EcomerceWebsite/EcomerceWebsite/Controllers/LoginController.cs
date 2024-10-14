b5d508126dcfe70dc394c412fa43e03b675e8482
Mynametrien <trienseventeen17@gmail.com>
2024-10-14 10:11:06 +0700

    the first of coding in project

---
+++ EcomerceWebsite/EcomerceWebsite/EcomerceWebsite/EcomerceWebsite/Controllers/LoginController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EcomerceWebsite.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            ViewBag.mess = TempData["mess"] as string;
+            return View();
+        }
+    }
+}
---
+++ EcomerceWebsite/EcomerceWebsite/EcomerceWebsite/EcomerceWebsite/Models/Account.cs
@@ -0,0 +1,60 @@
+namespace EcomerceWebsite.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("account")]
+    public partial class account
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public account()
+        {
+            carts = new HashSet<cart>();
+            Orders = new HashSet<Order>();
+            shipments = new HashSet<shipment>();
+            wishlists = new HashSet<wishlist>();
+        }
+
+        [Key]
+        public int account_id { get; set; }
+
+        [StringLength(100)]
+        public string first_name { get; set; }
+
+        [StringLength(100)]
+        public string last_name { get; set; }
+
+        [StringLength(100)]
+        public string email { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string password { get; set; }
+
+        [StringLength(100)]
+        public string address { get; set; }
+
+        public int? role { get; set; }
+
+        [StringLength(100)]
+        public string phone_number { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        public DateTime ngayTao { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<cart> carts { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Order> Orders { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<shipment> shipments { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<wishlist> wishlists { get; set; }
+    }
+}
